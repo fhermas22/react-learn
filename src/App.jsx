a4dec15e65461e005d5e5bb1d2a1501be2f89484
Hermas Francisco <franciscohermas@gmail.com>
2026-01-12 14:41:50 +0100

    Using useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,28 @@
-import { Fragment } from "react";
-
-const title = "Bonjour le monde !";
-const style = {
-  color: "red",
-  backgroundColor: "black",
-};
-const showTitle = false;
-const todos = [
-  "Apprendre React",
-  "Apprendre le JSX",
-  "Créer une application React",
-]
+import { Fragment, useState } from "react";
 
 function App() {
-  const handleClick = () => {
-    alert("J'ai cliqué !");
+  
+  const [person, setPerson] = useState({
+    firstname: 'John',
+    lastname: 'Doe',
+    age: 18
+  });
+
+  const [count, setCount] = useState(0);
+
+  const incrementAge = () => {
+    setPerson({...person, age: person.age + 1});
+  };
+
+  const incrementCount = () => {
+    setCount(count + 1);
   };
 
   return <>
-    {showTitle ? 
-      <h1 onClick={handleClick} id="title" className="title" style={style}>{title}</h1> : 
-      null
-    }
-    <Title color="green" id="monid" className="demo">Bonjour les gens</Title>
-
-    <input type="text" />
-
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis assumenda aliquid aperiam, repudiandae quasi optio error dolor ipsum eaque dignissimos expedita est. Est nostrum ut distinctio vero iure, voluptate hic?
-    </p>
-
-    <ul>
-      {todos.map(todo => (<li key={todo}>{todo}</li>))}
-    </ul>
-  </>;
-}
-
-function Title({color, children, hidden, ...props}) {
-
-  if (hidden) {
-    return null;
-  }
-
-  return <>
-    <h1 style={{color : color}} {...props}>{children}</h1>
-  </>;
+    <p>Age de {person.firstname} : {person.age }</p>
+    <button onClick={incrementAge}>Gagner une année</button>
+    <button onClick={incrementCount}>Incrémenter {count}</button>
+  </>
 }
 
 export default App
