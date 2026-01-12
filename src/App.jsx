7ad2b71ad6ae71925eae21867449d04ed16a46af
Hermas Francisco <franciscohermas@gmail.com>
2026-01-12 15:30:11 +0100

    Discovery of React Data Stream

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import { Fragment, useState } from "react";
 
 function App() {
 
-  const [value, setValue] = useState('');
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  }
-
-  const [checked, setChecked] = useState(true);
-  const toggleChecked = () => {
-    setChecked(!checked);
-  }
+  const [isTermsAccepted, setIsTermsAccepted] = useState(false);
 
   return <form>
-    <textarea value={value} onChange={handleChange} />
-    <input type="checkbox" checked={checked} onChange={toggleChecked} />
-    <button disabled={!checked}>Envoyer</button>
+    <CGUCheckbox checked={isTermsAccepted} onCheck={setIsTermsAccepted} />
+    <button disabled={!isTermsAccepted}>Envoyer le formulaire</button>
   </form>
 
+}
+
+function CGUCheckbox({ checked, onCheck }) {
+  return <div>
+    <label>
+      <input type="checkbox"
+        onChange={e => onCheck(e.target.checked)}
+        checked={checked} />
+      Accepter les conditions générales d'utilisation
+    </label>
+  </div>
 }
 
 export default App
